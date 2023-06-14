407257b20a10370d682bce6dc3a268f0b5b563ff
Mariam <Maf9894@gmail.com>
2023-06-14 19:22:48 +0300

    task done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const data = require("./data");
+const res = require("express/lib/response");
+
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send({
+    data: data,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
